5dea50ba227544f28ab4e0ea29555c8ecc9698cb
Jolin-ma <jolinma81@gmail.com>
2026-07-14 23:06:33 -0400

    add image gallery logic to script.js

--- imageGalley/script.js
+++ imageGalley/script.js
@@ -3,3 +3,42 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+const baseURL = "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
+
+const images = [
+  { filename: "pic1.jpg", alt: "Closeup of a human eye" },
+  { filename: "pic2.jpg", alt: "Rock that looks like a wave" },
+  { filename: "pic3.jpg", alt: "Purple and white pansies" },
+  { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
+  { filename: "pic5.jpg", alt: "Large moth on a leaf" },
+];
+
+function updateDisplayedImage(src, alt) {
+  displayedImage.src = src;
+  displayedImage.alt = alt;
+}
+
+for (const image of images) {
+  const newImage = document.createElement("img");
+  newImage.src = baseURL + image.filename;
+  newImage.alt = image.alt;
+  newImage.tabIndex = 0;
+
+  thumbBar.appendChild(newImage);
+
+  newImage.addEventListener("click", () => {
+    updateDisplayedImage(newImage.src, newImage.alt);
+  });
+}
+
+btn.addEventListener("click", () => {
+  if (btn.classList.contains("dark")) {
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 0.5)";
+  } else {
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 0)";
+  }
+  btn.classList.toggle("dark");
+});
